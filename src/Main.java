2c162a67264f6433b7f027c676f21b7c1e6b8542
João Victor Paltanin <joaovpaltanin116@gmail.com>
2026-07-22 13:59:52 -0300

    feat: inicia servidor java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.setExecutor(null);
+
+        server.start();
+        System.out.println("Servidor rodando em http://localhost:" + port);
+    }
+}
